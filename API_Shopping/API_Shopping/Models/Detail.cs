990914a74cbbb2accdf16270629539276872698f
dennis-gomez <dennis.gomez1310@gmail.com>
2026-01-27 08:49:15 -0600

    New models to order
    Creation of models to create a orders with the details of the order.

---
+++ API_Shopping/API_Shopping/Models/Detail.cs
@@ -0,0 +1,17 @@
+﻿namespace API_Shopping.Models
+{
+    public class Detail
+    {
+        public long ?Id { get; set; }
+        public int Quantity{ get; set; }
+        public int Price{ get; set; }
+        public int Total { get; set;  }
+        public DateTime? UpdateAt { get; set; }
+
+        public long OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public long ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ API_Shopping/API_Shopping/Models/DetailCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Shopping.Models
+{
+    public class DetailCreateDTO
+    {
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int Total { get; set; }
+        public long ProductId { get; set; }
+    }
+}
