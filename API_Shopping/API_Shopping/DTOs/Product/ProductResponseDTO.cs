c1e0dd01d856ff8e77167cb2ac6a2b0e191119a6
dennis-gomez <dennis.gomez1310@gmail.com>
2026-02-02 13:54:11 -0600

    Bug quantity
    The atribute quantity on product model is not showing

--- API_Shopping/API_Shopping/DTOs/Product/ProductResponseDTO.cs
+++ API_Shopping/API_Shopping/DTOs/Product/ProductResponseDTO.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public string Category { get; set; }
         public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
