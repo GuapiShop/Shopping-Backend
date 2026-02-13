fca7fbd12dabba02a97523d78ba603b210fa53a3
dennis-gomez <dennis.gomez1310@gmail.com>
2026-02-13 10:35:50 -0600

    ShoppingCartController Controller and get cart
    Creation of ShoppingCartController and the aggregation of GetShoppingCart endpoint to get a existing cart with items or create a new cart.

---
+++ API_Shopping/API_Shopping/Controllers/ShoppingCartController.cs
@@ -0,0 +1,44 @@
+﻿using API_Shopping.DTOs.Product;
+using API_Shopping.Interfaces;
+using API_Shopping.Models;
+using API_Shopping.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API_Shopping.Controllers
+{
+    [Route("api/shopping-cart")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public ShoppingCartController(IShoppingCartService shoppingCartService) {
+            this._shoppingCartService = shoppingCartService;
+        }
+
+        [HttpGet]
+        [Authorize(
+            AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,
+            Roles = "client"
+        )]
+        public async Task<ActionResult<ShoppingCart>> GetShoppingCart()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userIdClaim == null)
+            {
+                return Unauthorized();
+            }
+
+            long userId = long.Parse(userIdClaim);
+
+            var data = await _shoppingCartService.GetOrCreateShoppingCart(userId);
+
+            return Ok(data); 
+        }
+    }
+}
