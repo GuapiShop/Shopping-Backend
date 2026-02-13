0bd7f41fff8d01099beef7ae28028ddb5b846b97
dennis-gomez <dennis.gomez1310@gmail.com>
2026-02-12 18:27:47 -0600

    Models Shopping Cart
    Creation of models to add into the shopping cart, these are ShoppingCart, ItemShoppingCart and ItemShoppingCartCreateDTO.

---
+++ API_Shopping/API_Shopping/DTOs/ShoppingCart/ItemShoppingCartCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Shopping.DTOs.ShoppingCart
+{
+    public class ItemShoppingCartCreateDTO
+    {
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public long productId { get; set; }
+        public long shoppingCartId { get; set; }
+    }
+}
