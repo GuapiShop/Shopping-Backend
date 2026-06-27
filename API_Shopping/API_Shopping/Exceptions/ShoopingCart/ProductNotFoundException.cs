a9f6ff5c6ada23623915e1f9a546cb67a3014318
dennis-gomez <dennis.gomez1310@gmail.com>
2026-06-27 11:14:16 -0600

    [SCRUM-39] Duplicated Exception
    Delete product not found exception that is duplicated into Product and ShoppingCart.

--- API_Shopping/API_Shopping/Exceptions/ShoopingCart/ProductNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API_Shopping.Exceptions.ShoppingCart
-{
-    public class ProductNotFoundException : AppException
-    {
-        public ProductNotFoundException(long productId)
-            : base(StatusCodes.Status404NotFound, "Product not found", $"Product with ID {productId} was not found.") { }
-    }
-}
